c24bf2e8aeb1d6344f354cf67edc5760399d9ba8
sookiemonster <dsooknanan20@stuy.edu>
2021-01-04 12:12:31 -0500

    MakeStars.java prints stars instead of words.

--- MakeStars.java
+++ MakeStars.java
@@ -4,7 +4,17 @@ public class MakeStars {
   public static void main(String[] args) {
     Scanner n = new Scanner(System.in);
     while( n.hasNextLine()) {
-      System.out.println(n.nextLine());
+      Scanner s = new Scanner(n.nextLine());
+      while (s.hasNext()) {
+        int max = s.next().length();
+        for (int i = 0; i < max; i++) {
+          System.out.print("*");
+          if (i == max - 1) {
+            System.out.print(" ");
+          }
+        }
+      }
+      System.out.println();
     }
   }
 }
