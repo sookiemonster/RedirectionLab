e9ed84e7551393cfb26ae0d037cb5d7afd62087c
sookiemonster <dsooknanan20@stuy.edu>
2021-01-04 21:04:50 -0500

    Added pigLatin() and tests.:

--- Driver.java
+++ Driver.java
@@ -1,9 +1,18 @@
 public class Driver {
   public static void main(String[] args) {
+    // Simple Pig Latin
     System.out.println(PigLatin.pigLatinSimple("a"));
     System.out.println(PigLatin.pigLatinSimple("mock"));
     System.out.println(PigLatin.pigLatinSimple("pie"));
     System.out.println(PigLatin.pigLatinSimple("david"));
     System.out.println(PigLatin.pigLatinSimple("aaron"));
+    System.out.println();
+
+    // Pig Latin
+    System.out.println(PigLatin.pigLatin("the"));
+    System.out.println(PigLatin.pigLatin("check"));
+    System.out.println(PigLatin.pigLatin("skee"));
+    System.out.println(PigLatin.pigLatin("emu"));
+    System.out.println(PigLatin.pigLatin("grade"));
   }
 }
--- PigLatin.java
+++ PigLatin.java
@@ -11,4 +11,19 @@ public class PigLatin {
       return s.substring(1, s.length()) + s.substring(0,1) + "ay";
     }
   }
+
+  public static String pigLatin(String s) {
+    String[] digraphTemp = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    ArrayList<String> digraph = new ArrayList<String>(Arrays.asList(digraphTemp));
+    String[] vowelsTemp = {"a", "e", "i", "o", "u"};
+    ArrayList<String> vowels = new ArrayList<String>(Arrays.asList(vowelsTemp));
+    s = s.toLowerCase();
+    if (vowels.contains(s.substring(0, 1))) {
+      return s + "hay";
+    } else if (digraph.contains(s.substring(0,2))) {
+      return s.substring(2, s.length()) + s.substring(0, 2) + "ay";
+    } else {
+      return s.substring(1, s.length()) + s.substring(0,1) + "ay";
+    }
+  }
 }
